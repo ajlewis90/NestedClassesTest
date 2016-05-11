036f4c8d1d621681b2e5f52bac115e628b45d20c
ajlewis90 <Joel@HP-PC>
2016-05-11 15:32:12 +1200

    Partial implementation of displaying the numbers in printStatement() of Class1

--- NestedClassTest/src/Class1.java
+++ NestedClassTest/src/Class1.java
@@ -1,11 +1,17 @@
 
 public class Class1 {
-	
+
+	int data[] = {0, 4, 42};
+
 	public void printStatement(){
-	System.out.println("Hello World");
-	
-	System.out.println("Hello World again");
-	
-	System.out.println("Hello World again 2");
+		System.out.println("Hello World");
+
+		System.out.println("Hello World again");
+
+		System.out.println("Hello World again 2");
+
+		for (int i = 0; i < data.length; i++){
+			System.out.println("Hello World i value is: " + i);
+		}
 	}
 }
